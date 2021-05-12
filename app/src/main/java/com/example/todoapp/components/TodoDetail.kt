abe1135e87c45a54e1943bf43e24f28d0ed7c628
mariodz95 <mariodz95@gmail.com>
2021-05-12 09:58:49 +0200

    Display todo details

--- app/src/main/java/com/example/todoapp/components/TodoDetail.kt
+++ app/src/main/java/com/example/todoapp/components/TodoDetail.kt
@@ -1,21 +1,22 @@
 package com.example.todoapp.components
 
 import android.util.Log
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.example.todoapp.database.entity.Todo
 import com.example.todoapp.model.TodoViewModel
 import com.google.gson.Gson
+import kotlinx.coroutines.launch
 
 @Composable
 fun TodoDetailContent(navController: NavHostController, todoString: String?, todoViewModel: TodoViewModel) {
@@ -29,7 +30,8 @@ fun TodoDetailContent(navController: NavHostController, todoString: String?, tod
     TodoDetail(
         navController = navController,
         todo = todo,
-        deleteTodo = {todoViewModel.deleteTodo(it)}
+        deleteTodo = {todoViewModel.deleteTodo(it)},
+        checkTodo = {todoViewModel.checkTodo(it)}
     )
 
 }
@@ -38,7 +40,8 @@ fun TodoDetailContent(navController: NavHostController, todoString: String?, tod
 fun TodoDetail(
     navController: NavHostController,
     todo: Todo?,
-    deleteTodo: (Todo) -> Unit
+    deleteTodo: (Todo) -> Unit,
+    checkTodo: (Todo) -> Unit
 ){
     val materialBlue700= Color(0xFF1976D2)
     Scaffold(
@@ -72,6 +75,57 @@ fun TodoDetail(
 
             )
         },
-        content = { Text("BodyContent") },
+        floatingActionButtonPosition = FabPosition.End,
+        floatingActionButton = {
+            FloatingActionButton(
+                modifier = Modifier.padding(8.dp),
+                onClick =  {
+                    checkTodo(todo!!)
+                    navController.popBackStack()
+                },
+                backgroundColor = Color(0xFF1976D2),
+                shape = CircleShape,
+            ){
+                Icon(Icons.Filled.Done, "")
+            }
+                               },
+        content = {
+            Column(
+                modifier = Modifier.padding(24.dp)
+            ){
+                Row(
+                    modifier = Modifier.padding(8.dp)
+                ){
+                    Text("${todo?.name}", fontWeight = FontWeight.Bold, fontSize = 24.sp)
+                }
+
+                Row(
+                    modifier = Modifier.padding(8.dp)
+                ){
+                    if(todo?.details == ""){
+                        Text("No details!", fontSize = 20.sp)
+
+                    }else{
+                        Text("Details: ${todo?.details}", fontSize = 20.sp)
+                    }
+                }
+                Row(
+                    modifier = Modifier.padding(8.dp)
+                ){
+                    if(todo?.category == null){
+                        Text("No category!")
+                    }else{
+                        Text("${todo?.category}")
+
+                    }
+                }
+
+                Row(
+                    modifier = Modifier.padding(8.dp)
+                ){
+                    Text("${todo?.dateAdded}")
+                }
+            }
+                  },
         )
 }
